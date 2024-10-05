30f86b12f741131ae5ae36082f8e57d339f54083
Timur Meyliev <Timka654work@gmail.com>
2024-10-06 02:23:32 +0300

    add docker publish/restart code to onEnd script template

--- ServerPublisher.Server.Scripts/data/project_template/scripts/OnEnd.cs
+++ ServerPublisher.Server.Scripts/data/project_template/scripts/OnEnd.cs
@@ -2,13 +2,32 @@ public partial class PublisherScript
 {
     public static void OnEnd(ScriptInvokingContext context, bool success, bool postProcessingSuccess, Dictionary<string, string> args)
     {
-        if (success && postProcessingSuccess)
-        {
-            //var execPath = Path.Combine(ScriptCore.ProjectInfo.ProjectDirPath,"testapp");
+        //if (success && postProcessingSuccess)
+        //{
+        //    //var execPath = Path.Combine(ScriptCore.ProjectInfo.ProjectDirPath,"testapp");
 
-            //Utils.BashExec($"sudo chmod +x '{execPath}'");
+        //    //Utils.BashExec($"sudo chmod +x '{execPath}'");
 
-            //Utils.BashExec("sudo systemctl start test.service");
-        }
+        //    //Utils.BashExec("sudo systemctl start test.service");
+        //}
+        //if (success && postProcessingSuccess && context.Executor.AnyFiles())
+        //{
+        //    //// create/upload docker image
+        //    //Utils.CmdExec(context, [
+        //    //    $"cd \"{context.Project.ProjectDirPath}\"",
+        //    //    "docker build -t dotnet_instance -f Dockerfile .",
+        //    //    "docker tag dotnet_instance:latest localhost:5000/dotnet_instance",
+        //    //    "docker push localhost:5000/dotnet_instance"]);
+
+        //    //// reboot instance with update
+        //    //Utils.CmdExec(context, [
+        //    //    "cd \"/root/dotnet_instance\"",
+        //    //    "docker compose pull",
+        //    //    "docker compose down",
+        //    //    "sleep 2",
+        //    //    "docker compose up --force-recreate --build -d",
+        //    //    "docker image prune -f"
+        //    //]);
+        //}
     }
 }
