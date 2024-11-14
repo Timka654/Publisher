627262f6493c6e7f3d06f5fa5f8f56ae2297b1bf
Timur Meyliev <Timka654work@gmail.com>
2024-11-14 16:02:42 +0200

    rename project

--- NSL.Deploy.QuickUpdater/Resource.Designer.cs
+++ NSL.Deploy.QuickUpdater/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuickUpdater {
+namespace NSL.Deploy.QuickUpdater {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace QuickUpdater {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("QuickUpdater.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("NSL.Deploy.QuickUpdater.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
